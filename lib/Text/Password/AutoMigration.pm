3c713b6e6992d4bb4acf2eea2b3bc6206065dd1c
Yuki Yoshida <worthmine@gmail.com>
2017-08-09 22:11:47 +0900

    fixed the grammar in PODs

--- lib/Text/Password/AutoMigration.pm
+++ lib/Text/Password/AutoMigration.pm
@@ -24,7 +24,7 @@ Text::Password::AutoMigration is the Module for lasy Administrators.
 
 It always generates the password with SHA512.
  
-And verifies Automatically the hash with
+And verifies automatically the hash with
 B<CORE::crypt>, B<MD5>, B<SHA-1 by hex>, B<SHA-256> and of course B<SHA-512>.
 
 All you have to do are those:
@@ -43,7 +43,7 @@ No arguments are required. But you can set some parameters.
 
 =item default
 
-You can set default length with param 'default' like below
+You can set default length with param 'default' like below:
 
  $pwd = Text::Pasword::AutoMiglation->new( default => 12 );
 
@@ -55,7 +55,7 @@ Or you can set default strength for password with param 'readablity'.
 
 It must be a Boolean, defaults to 1.
 
-If it was set as 0, you can generate stronger passwords with generate()
+If it was set as 0, you can generate stronger passwords with generate().
 
  $pwd = Text::Pasword::AutoMiglation->new( readability => 0 );
 
@@ -66,7 +66,7 @@ It must be a Boolean, defaults to 1.
 This module is for Administrators who try to replace hashes in their DB.
 However, if you've already done to replace them or start to make new Apps with this module,
 you can set param migrate as 0. 
-Then it will work a little faster to skip making alternative hashes.
+Then it will work a little faster without regenerating new hashes.
 
 =cut
 
@@ -78,11 +78,11 @@ has migrate => ( is => 'rw', isa => 'Bool', default => 1 );
 
 =head3 verify( $raw, $hash )
 
-returns true value if the verify is success
+returns the true value if the verification succeeds.
 
-Actually, the value is new hash with SHA-512 from $raw
+Actually, the value is new hash with SHA-512 from $raw.
 
-So you can replace hashes in your DB very easily like below
+So you can replace hashes in your DB very easily like below:
  
  my $pwd = Text::Password::AutoMigration->new();
  my $input = $req->body_parameters->{passwd};
@@ -117,24 +117,24 @@ override 'verify' => sub {
 
 =head3 nonce($length)
 
-generates the strings with enough strength
+generates the strings with enough strength.
 
-the length defaults to 8($self->default)
+the length defaults to 8($self->default).
 
 =head3 encrypt($raw)
 
-returns hash with unix_sha512_crypt()
+returns hash with unix_sha512_crypt().
 
-salt will be made automatically
+salt will be made automatically.
  
 =head3 generate($length)
 
-genarates pair of new password and it's hash
+genarates pair of new password and it's hash.
 
 less readable characters(0Oo1Il|!2Zz5sS$6b9qCcKkUuVvWwXx.,:;~-^'"`) are forbidden
 unless $self->readability is 0.
 
-the length defaults to 8($self->default)
+the length defaults to 8($self->default).
  
 =cut
 

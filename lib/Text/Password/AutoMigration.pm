c98a261af843d84be3ce7284d0cb1ab8815aded7
Yuki Yoshida <worthmine@gmail.com>
2017-08-21 22:04:57 +0900

    fixed the typo in POD

--- lib/Text/Password/AutoMigration.pm
+++ lib/Text/Password/AutoMigration.pm
@@ -98,7 +98,7 @@ So you can replace hashes in your DB very easily like below:
 
 New hash length is at least 98. So you have to change your DB like below:
 
- ALTER TABLE User CHANGE paswwd passwd VARCHAR(98);
+ ALTER TABLE User CHANGE passwd passwd VARCHAR(98);
 
 =cut
 
